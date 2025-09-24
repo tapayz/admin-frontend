3d505e6df736d1c8383eae9de58db711b026b224
ajossyvibe <ajossyvibe@gmail.com>
2025-09-24 15:27:23 +0800

    - bug resolve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,13 +6,9 @@ module.exports = {
       args: 'start',                 // next start (프로덕션)
       watch: false,
       env: {
-        NODE_ENV: 'development',
-        PORT: 3000,                  // 개발이라도 Nginx 뒤에선 3000 고정 권장
-      },
-      env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,                  // 운영도 3000
-      },
+        PORT: 3000,                  // 개발이라도 Nginx 뒤에선 3000 고정 권장
+      }
     },
   ],
 };
